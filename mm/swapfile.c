bd43001c8ae8767009cd21917cbbb812990b3060
bipindr123 <bipindr123@gmail.com>
2019-01-19 23:37:36 +0530

    Revert "Kolivas drop swap cache aggressively"
    This reverts commit a6b979de5b997f7240ba66a58934271e343cc113.

--- mm/swapfile.c
+++ mm/swapfile.c
@@ -309,7 +309,7 @@ checks:
 		scan_base = offset = si->lowest_bit;
 
 	/* reuse swap entry of cache-only swap if not busy. */
-	if (si->swap_map[offset] == SWAP_HAS_CACHE) { 
+	if (vm_swap_full(si) && si->swap_map[offset] == SWAP_HAS_CACHE) {
 		int swap_was_freed;
 		spin_unlock(&swap_lock);
 		swap_was_freed = __try_to_reclaim_swap(si, offset);
@@ -398,7 +398,8 @@ scan:
 			spin_lock(&swap_lock);
 			goto checks;
 		}
-		if (si->swap_map[offset] == SWAP_HAS_CACHE) {
+		if (vm_swap_full(si) &&
+			si->swap_map[offset] == SWAP_HAS_CACHE) {
 			spin_lock(&swap_lock);
 			goto checks;
 		}
@@ -413,7 +414,8 @@ scan:
 			spin_lock(&swap_lock);
 			goto checks;
 		}
-		if (si->swap_map[offset] == SWAP_HAS_CACHE) {
+		if (vm_swap_full(si) &&
+			si->swap_map[offset] == SWAP_HAS_CACHE) {
 			spin_lock(&swap_lock);
 			goto checks;
 		}
@@ -727,7 +729,9 @@ int free_swap_and_cache(swp_entry_t entry)
 		 * Not mapped elsewhere, or swap space full? Free it!
 		 * Also recheck PageSwapCache now page is locked (above).
 		 */
-		if (PageSwapCache(page) && !PageWriteback(page)) {
+		if (PageSwapCache(page) && !PageWriteback(page) &&
+				(!page_mapped(page) ||
+				vm_swap_full(page_swap_info(page)))) {
 			delete_from_swap_cache(page);
 			SetPageDirty(page);
 		}
